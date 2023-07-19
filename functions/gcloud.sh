15d08f9882c6a5c35d6127d24691205cdc1e69f7
titooo7 <juanmartinbravo@gmail.com>
2023-07-19 12:15:01 +0200

    Update gcloud.sh

--- functions/gcloud.sh
+++ functions/gcloud.sh
@@ -7,7 +7,7 @@
 ################################################################################
 gcloud () {
   export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-  echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-  sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+  sudo apt-get update && sudo apt-get install google-cloud-cli
 }
